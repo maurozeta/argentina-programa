56efbb62023107045ca22c1e049b01e8ebc4558c
maurozeta <maurozacaria2021@gmail.com>
2023-08-28 12:09:42 -0300

    Agregado tarea 4
    Finalización y agregado de la tarea 4 de la clase 5

--- clase5/tarea4/javascript.js
+++ clase5/tarea4/javascript.js
@@ -8,4 +8,69 @@
 
 const $listaNumeros = document.querySelectorAll('li');
 
-const numerosListados = [];
+const $promedio = document.querySelector('#promedio');
+
+const $numeroMasPequeno = document.querySelector('#numero-mas-pequeno');
+
+const $numeroMasGrande = document.querySelector('#numero-mas-grande');
+
+const $numeroMasFrecuente = document.querySelector('#numero-mas-frecuente');
+
+const $calcularDatos = document.querySelector('#calcular-datos');
+
+const listaDeNumeros = [];
+
+$listaNumeros.forEach((contenido) => {
+    const contenidoLista = parseFloat(contenido.textContent);
+    listaDeNumeros.push(contenidoLista);
+});
+
+
+function calcularPromedio(listaDeNumeros) {
+    let promedioLista = 0;
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+        promedioLista = promedioLista + listaDeNumeros[i];       
+    }
+    return promedioLista / listaDeNumeros.length;
+}
+
+function calcularNumeroMasPequeno(listaDeNumeros) {
+    let numeroMasPequeno = listaDeNumeros[0];
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+            if (listaDeNumeros[i] < numeroMasPequeno) {
+                numeroMasPequeno = listaDeNumeros[i];
+            }
+    }
+    return numeroMasPequeno;
+}
+
+function calcularNumeroMasGrande(listaDeNumeros) {
+    let numeroMasGrande = listaDeNumeros[0];
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+            if (listaDeNumeros[i] > numeroMasGrande) {
+                numeroMasGrande = listaDeNumeros[i];
+            }
+    }
+    return numeroMasGrande;
+}
+
+function calcularNumeroMasFrecuente(listaDeNumeros) {
+    let numeroMasFrecuente = [];
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+        for (let j = i + 1; j < listaDeNumeros.length; j++) {
+            if (listaDeNumeros[i] === listaDeNumeros[j] && !numeroMasFrecuente.includes(listaDeNumeros[i])) {
+                numeroMasFrecuente.push(listaDeNumeros[i]);
+            }
+        }
+    }
+    return numeroMasFrecuente;
+}
+
+$calcularDatos.onclick = function() {
+    $promedio.textContent = `El promedio es: ${calcularPromedio(listaDeNumeros)}`;
+    $numeroMasPequeno.textContent = `El número más pequeño es: ${calcularNumeroMasPequeno(listaDeNumeros)}`;
+    $numeroMasGrande.textContent = `El número más grande es: ${calcularNumeroMasGrande(listaDeNumeros)}`;
+    $numeroMasFrecuente.textContent = `El número más frecuente es: ${calcularNumeroMasFrecuente(listaDeNumeros)}`;
+
+    return false;
+}
